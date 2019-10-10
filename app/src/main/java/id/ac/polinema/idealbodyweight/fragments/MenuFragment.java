16edc42133a4ea6b083973bef693a9be8c5ade0b
ajiputra19 <ajiputrawicaksanaa19@gmail.com>
2019-10-10 21:19:51 +0700

    Praktikum 2 Fragment 6 Perubahan 2

--- app/src/main/java/id/ac/polinema/idealbodyweight/fragments/MenuFragment.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/fragments/MenuFragment.java
@@ -1,6 +1,7 @@
 package id.ac.polinema.idealbodyweight.fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import id.ac.polinema.idealbodyweight.R;
 
@@ -16,22 +18,65 @@ import id.ac.polinema.idealbodyweight.R;
  */
 public class MenuFragment extends Fragment {
 
-
+    private OnFragmentInteractionListener mListener;
     public MenuFragment() {
         // Required empty public constructor
     }
 
 
-    public interface OnFragmentInteractionListener {
-        void onBrocaIndexButtonClicked();
-        void onBodyMassIndexButtonClicked();
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        View view = inflater.inflate(R.layout.fragment_menu, container, false);
+
+        // sisipkan register event click nanti di sini.
+        Button brocaButton = view.findViewById(R.id.button_broca_index);
+        Button bmiButton = view.findViewById(R.id.broca_bmi);
+
+        brocaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mListener != null) {
+                    mListener.onBrocaIndexButtonClicked();
+                }
+            }
+        });
+
+        bmiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mListener != null) {
+                    mListener.onBodyMassIndexButtonClicked();
+                }
+            }
+        });
+
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onBrocaIndexButtonClicked();
+        void onBodyMassIndexButtonClicked();
     }
 
 }
